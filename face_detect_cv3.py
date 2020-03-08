12ecfe6be26fcb6c46c510ae33796b609ff4d0b9
Shaneli2 <276433574@qq.com>
2020-03-08 18:41:33 +0800

    little changes on *_cv3.py

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -11,11 +11,13 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 # Read the image
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#cv2.imshow("cvt to grary", gray)
+#cv2.waitKey(0)
 
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.1,
+    scaleFactor=1.28,
     minNeighbors=5,
     minSize=(30, 30)
     #flags = cv2.CV_HAAR_SCALE_IMAGE
